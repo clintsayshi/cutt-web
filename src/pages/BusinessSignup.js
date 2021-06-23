9f84afdfd430b65760e721cb9bce6e2d377e07e0
mcclintsayshi <manakanejustin@gmail.com>
2021-06-23 12:15:29 +0200

    started auth with firebase

---
+++ src/pages/BusinessSignup.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+const BSignup = () => {
+  return <div></div>;
+};
+
+export default BSignup;
+
+/* 
+    Name
+    Categories
+    Location
+    Services (including Photos)
+    Description
+*/
+
+const Input = styled.input`
+  padding: .5rem;
+
+  font-family: 'poppins', sans-serif;
+  font-size: 1rem;
+  font-weight: normal;
+  color: black;
+`;
+
+const 
